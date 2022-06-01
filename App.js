ad943f2be3ac0beec893d5589de705225abaf2b9
Dyras <johan.forsberg@my.com>
2022-06-01 12:06:09 +0200

    Laddar upp början

--- App.js
+++ App.js
@@ -1,12 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import {useState } from "react";
 
 export default function App() {
+  
+  const [textInputValue, setTextInputValue] = useState('')
+
+  const handleTextChange = (text) => {
+    setTextInputValue(text);
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <Text style={styles.title}>Sten sax påse</Text>
+      <View style={styles.direction}>
+      <Button title="Sten" style={styles.button} onPress={() => {}}></Button>
+      <Button title="Sax" style={styles.button} onPress={() => {}}></Button>
+
+      <Button title="Påse" style={styles.button} onPress={() => {}}></Button>
+
+      </View>
+      </View>
   );
 }
 
@@ -14,7 +28,18 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    alignItems: 'center'
+  }, direction: {
+    flexDirection: 'row'
+  }, title: {
+    marginTop: 50,
+    fontSize: 30
+  }, textinput: {
+    width: '70%',
+    marginHorizontal: 20,
+    paddingHorizontal: 10
+  }, button: {
+    marginBottom: 20,
+    padding: 30
+  }
 });
